baffde4bc8ac5aea9c9b00784a1669b790f08824
Jessienguyen <67177733+jessienguyen298@users.noreply.github.com>
2021-06-23 18:15:46 +0700

    Update Basket of goods analysis.sql

--- Basket of goods analysis.sql
+++ Basket of goods analysis.sql
@@ -3,6 +3,43 @@ Basket of goods analysis: A common retail analytics task is to analyze each bask
 In this time, report the names of products that appear in the same order ten or more times.
 */
 
+USE Salesdata;
+
+--create pair orders table
+SELECT * INTO #temporder 
+FROM (
+	SELECT od1.orderNumber, CONCAT(od1.productCode,' ', od2.productCode) AS items
+	FROM (SELECT DISTINCT productCode, orderNumber FROM orderdetails) od1
+	JOIN (SELECT DISTINCT productCode, orderNumber FROM orderdetails) od2
+	ON od1.orderNumber = od2.orderNumber
+	WHERE od1.productCode != od2.productCode AND od1.productCode < od2.productCode
+	) temporder;
+
+--create showing frequency	
+SELECT * INTO #frequency
+FROM(
+SELECT items, count(*) as frequency FROM #temporder
+GROUP BY items
+) frequency;
+
+--filter basket items which have been often bought together more than 10 times 
+SELECT * INTO #frequencyitem 
+FROM 
+(SELECT RTRIM(LEFT(items, 9)) as item1, LTRIM(RIGHT(items, 9)) as item2
+FROM #frequency
+WHERE frequency >= 10) frequencyitem 
+
+ --showing pair basket items and its name
+ SELECT item1, p1.productName, item2, p2.productName
+ FROM #frequencyitem
+ JOIN products p1 ON item1 = p1.productCode
+ JOIN products p2 ON item2 = p2.productCode;
+
+ DROP TABLE #temporder;
+ DROP TABLE #frequency;
+ DROP TABLE #frequencyitem;
+
+
 
 
 
