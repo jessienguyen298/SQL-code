5d4383ca0c961b48ab0389f632bab502fa858898
Jessienguyen <67177733+jessienguyen298@users.noreply.github.com>
2021-06-23 17:13:51 +0700

    Create Basket of goods analysis.sql

---
+++ Basket of goods analysis.sql
@@ -0,0 +1,8 @@
+/*
+Basket of goods analysis: A common retail analytics task is to analyze each basket or order to learn what products are often purchased together. 
+In this time, report the names of products that appear in the same order ten or more times.
+*/
+
+
+
+
