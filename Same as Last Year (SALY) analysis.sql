d3e912cf6359a5e765993176d67aa17b5ca51419
Jessienguyen <67177733+jessienguyen298@users.noreply.github.com>
2021-06-24 18:05:55 +0700

    Update Same as Last Year (SALY) analysis.sql

--- Same as Last Year (SALY) analysis.sql
+++ Same as Last Year (SALY) analysis.sql
@@ -4,8 +4,12 @@ skills: CTE, Join, Case when
 */
 	 
 SELECT od.productCode,
-       SUM(CASE WHEN year(orderDate) = 2004 THEN quantityOrdered*priceEach END) AS sales_2003,
-       SUM(CASE WHEN year(orderDate) = 2003 THEN quantityOrdered*priceEach END) AS sales_2004
+       SUM(CASE WHEN year(orderDate) = 2003 THEN quantityOrdered*priceEach END) AS sales_2003,
+       SUM(CASE WHEN year(orderDate) = 2004 THEN quantityOrdered*priceEach END) AS sales_2004,
+	   ROUND(
+	   (100*SUM(CASE WHEN year(orderDate) = 2003 THEN quantityOrdered*priceEach END)
+	   / SUM(CASE WHEN year(orderDate) = 2004 THEN quantityOrdered*priceEach END))
+	   ,2) as percentagechange
 FROM orderdetails od 
 JOIN orders o ON o.orderNumber = od.orderNumber
 GROUP BY od.productCode;
