ebbd0a53861c28aa69e6c68e40421d47d5d82c87
Jessienguyen <67177733+jessienguyen298@users.noreply.github.com>
2021-06-24 17:15:42 +0700

    Update Same as Last Year (SALY) analysis.sql

--- Same as Last Year (SALY) analysis.sql
+++ Same as Last Year (SALY) analysis.sql
@@ -1,25 +1,15 @@
 /*
 Same as Last Year (SALY) analysis: concept to calculate sales revenue of a product code this year compare with the previous year, incase compare 2004 with 2003
-skills: CTE, Join, temp_table
+skills: CTE, Join, Case when
 */
-
-SELECT od.productCode,sum(quantityOrdered*priceEach) as sales_2003 INTO #sales2003
-FROM orderdetails  od 
-JOIN orders o ON o.orderNumber =od.orderNumber 
-WHERE  orderDate BETWEEN '2002-12-31' AND '2004-01-01'
+	 
+SELECT od.productCode,
+       SUM(CASE WHEN year(orderDate) = 2004 THEN quantityOrdered*priceEach END) AS sales_2003,
+       SUM(CASE WHEN year(orderDate) = 2003 THEN quantityOrdered*priceEach END) AS sales_2004
+FROM orderdetails od 
+JOIN orders o ON o.orderNumber = od.orderNumber
 GROUP BY od.productCode;
 
-SELECT od.productCode,sum(quantityOrdered*priceEach) as sales_2004 INTO #sales2004
-FROM orderdetails  od 
-JOIN orders o ON o.orderNumber =od.orderNumber 
-WHERE  orderDate BETWEEN '2003-12-31' AND '2005-01-01'
-GROUP BY od.productCode;
 
-Select * from #sales2003;
-Select * from #sales2004;
 
-SELECT s3.productCode, sales_2003, sales_2004, round((100*sales_2003/sales_2004),2) as percentagechange
-FROM #sales2003 s3 LEFT JOIN #sales2004 s4 ON s3.productCode = s4.productCode;
 
-DROP TABLE #sales2003;
-DROP TABLE #sales2004;
